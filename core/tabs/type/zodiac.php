e23d49e2ab481b3eed579bceece3a020859c7415
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-09-21 18:43:40 +0300

    update tab

--- core/tabs/type/zodiac.php
+++ core/tabs/type/zodiac.php
@@ -55,5 +55,37 @@ class zodiac extends base
 	public function load(string $username): void
 	{
 		$member = $this->get_user_data($username);
+
+		$u_bday = $member['user_birthday'];
+
+		if ($this->config['allow_birthdays'] && $u_bday && $this->config['gzo_zodiac'])
+		{
+			$this->language->add_lang(['zodiac', 'astro'], 'ganstaz/zodiac');
+
+			// Format date
+			$u_bday = str_replace(' ', '', $u_bday);
+			$date = \DateTime::createFromFormat('d-m-Y', $u_bday);
+
+			foreach ($this->manager->get_zodiac_types() as $zodiac_type)
+			{
+				$zodiac = $this->manager->get($zodiac_type);
+
+				foreach ($zodiac->load($date->format($zodiac->get_format())) as $row)
+				{
+					$this->template->assign_block_vars('zodiac_data', [
+						'stem'	  => $row['stem'],
+						'sign'	  => $row['sign'],
+						'symbol'  => $row['symbol'],
+						'plant'	  => $row['plant'],
+						'gem'	  => $row['gem'],
+						'ruler'	  => $row['ruler'],
+						'extra'	  => $row['extra'],
+						'dir'	  => $row['dir'],
+						'element' => $row['element'],
+						'name'	  => $row['name'],
+					]);
+				}
+			}
+		}
 	}
 }
