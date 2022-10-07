a5f458ab8e194e1f077d6199d82537ea1c23965d
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-10-08 00:25:36 +0300

    fix year

--- core/helper.php
+++ core/helper.php
@@ -3,7 +3,7 @@
 *
 * GZO Web. An extension for the phpBB Forum Software package.
 *
-* @copyright (c) 2021, GanstaZ, http://www.github.com/GanstaZ/
+* @copyright (c) 2022, GanstaZ, http://www.github.com/GanstaZ/
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
